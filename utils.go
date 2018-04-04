d6088b136592c506be03b54e0d9b44d6d57c1989
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-04-04 20:08:54 +0300

    FloorLog2 function

--- utils.go
+++ utils.go
@@ -4,15 +4,16 @@ import (
 	"math/big"
 )
 
-var ONE = big.NewInt(0)
+var ONE = big.NewInt(1)
 
 func init() {
 }
 
-func FloorLog2(n *big.Int) *big.Int {
+func FloorLog2(val *big.Int) *big.Int {
 	res := new(big.Int)
+	n := (&big.Int{}).Set(val)
 
-	if res.Cmp(big.NewInt(256)) == -1 {
+	if n.Cmp(big.NewInt(256)) == -1 {
 		// At most 8 iterations
 		for n.Cmp(big.NewInt(1)) == 1 {
 			n.Rsh(n, 1)
@@ -21,8 +22,7 @@ func FloorLog2(n *big.Int) *big.Int {
 	} else {
 		// Exactly 8 iterations
 		for s := 128; s > 0; s = s >> 1 {
-			t := big.NewInt(0)
-			t.Set(ONE)
+			t := (&big.Int{}).Set(ONE)
 			t.Lsh(t, uint(s))
 			if n.Cmp(t) >= 0 {
 				n.Rsh(n, uint(s))
--- utils_test.go
+++ utils_test.go
@@ -1,16 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"math/big"
 	"testing"
 )
 
 func TestFloorLog2(t *testing.T) {
+	bigOne := big.NewInt(1)
 	for n := 1; n <= 255; n++ {
-		input := big.NewInt(2)
+		input1 := big.NewInt(2)
+		input1.Exp(input1, big.NewInt(int64(n)), nil)
+
+		input2 := big.NewInt(2)
+		input2.Exp(input2, big.NewInt(int64(n)), nil)
+		input2.Add(input2, bigOne)
+
+		input3 := big.NewInt(2)
+		input3.Exp(input2, big.NewInt(int64(n+1)), nil)
+		input3.Sub(input2, bigOne)
+
 		output := big.NewInt(int64(n))
-		input.Exp(input, big.NewInt(int64(n)), nil)
-		fmt.Println(input, " ", FloorLog2(input), " ", output)
+		res := FloorLog2(input1)
+		if res.Cmp(output) != 0 {
+			t.Errorf("Error FloorLog2: expected %s, got %s", output, res)
+		}
+		res = FloorLog2(input2)
+		if res.Cmp(output) != 0 {
+			t.Errorf("Error FloorLog2: expected %s, got %s", output, res)
+		}
+		res = FloorLog2(input3)
+		if res.Cmp(output) != 0 {
+			t.Errorf("Error FloorLog2: expected %s, got %s", output, res)
+		}
 	}
 }
