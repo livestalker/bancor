fb47e2329f5e55ce46ebed80d04833a4c53e2b69
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-04-06 15:59:49 +0300

    work on helper array

--- testhelpers.go
+++ testhelpers.go
@@ -5,10 +5,18 @@ import (
 	"math/big"
 )
 
-var HelperStrMaxExpArray [128]string
-var HelperMaxExpArray [128]*big.Int
+var (
+	HelperMaxExpArray [128]*big.Int
+	HelperMaxValArray [128]*big.Int
+)
 
 func init() {
+	initHelperMaxExpArray()
+	initHelperMaxValArray()
+}
+
+func initHelperMaxExpArray() {
+	var HelperStrMaxExpArray [128]string
 	HelperStrMaxExpArray[0] = "d7"
 	HelperStrMaxExpArray[1] = "019f"
 	HelperStrMaxExpArray[2] = "031b"
@@ -141,134 +149,140 @@ func init() {
 		b, _ := hex.DecodeString(HelperStrMaxExpArray[i])
 		HelperMaxExpArray[i] = (&big.Int{}).SetBytes(b)
 	}
-	//module.exports.maxValArray = [
-	//    /*   0 */    '0x2550a7d99147ce113d27f304d24a422c3d',
-	//    /*   1 */    '0x1745f7d567fdd8c93da354496cf4dddf34',
-	//    /*   2 */    '0xb5301cf4bf20167721bcdbe218a66f1e0',
-	//    /*   3 */    '0x5e2d2ca56fae9ef2e524ba4d0f75b8754',
-	//    /*   4 */    '0x2f45acad795bce6dcd748391bb828dcea',
-	//    /*   5 */    '0x17f631b6609d1047920e1a1f9613f870d',
-	//    /*   6 */    '0xc29d4a7745ae89ef20a05db656441649',
-	//    /*   7 */    '0x6242dea9277cf2d473468985313625bb',
-	//    /*   8 */    '0x31aef9b37fbc57d1ca51c53eb472c345',
-	//    /*   9 */    '0x1923b23c38638957faeb8b4fe57b5ead',
-	//    /*  10 */    '0xcb919ec79bf364210433b9b9680eadd',
-	//    /*  11 */    '0x67186c63186761709a96a91d44ff2bf',
-	//    /*  12 */    '0x343e6242f854acd626b78022c4a8002',
-	//    /*  13 */    '0x1a7efb7b1b687ccb2bb413b92d5e413',
-	//    /*  14 */    '0xd72d0627fadb6aa6e0f3c994a5592a',
-	//    /*  15 */    '0x6d4f32a7dcd0924c122312b7522049',
-	//    /*  16 */    '0x37947990f145344d736c1e7e5cff2f',
-	//    /*  17 */    '0x1c49d8ceb31e3ef3e98703e0e656cc',
-	//    /*  18 */    '0xe69cb6255a180e2ead170f676fa3c',
-	//    /*  19 */    '0x75a24620898b4a19aafdfa67d23e8',
-	//    /*  20 */    '0x3c1419351dd33d49e1ce203728e25',
-	//    /*  21 */    '0x1eb97e709f819575e656eefb8bd98',
-	//    /*  22 */    '0xfbc4a1f867f03d4c057d522b6523',
-	//    /*  23 */    '0x812507c14867d2237468ba955def',
-	//    /*  24 */    '0x425b9d8ca5a58142d5172c3eb2b5',
-	//    /*  25 */    '0x2228e76a368b75ea80882c9f6010',
-	//    /*  26 */    '0x119ed9f43c52cdd38348ee8d7b23',
-	//    /*  27 */    '0x91bfcff5e91c7f115393af54bad',
-	//    /*  28 */    '0x4b8845f19f7b4a93653588ce846',
-	//    /*  29 */    '0x273fa600431f30b0f21b619c797',
-	//    /*  30 */    '0x1474840ba4069691110ff1bb823',
-	//    /*  31 */    '0xab212322b671a11d3647e3ecaf',
-	//    /*  32 */    '0x59ce8876bf3a3b1b396ae19c95',
-	//    /*  33 */    '0x2f523e50d3b0d68a3e39f2f06e',
-	//    /*  34 */    '0x190c4f51698c5ee5c3b34928a0',
-	//    /*  35 */    '0xd537c5d5647f2a79965d56f94',
-	//    /*  36 */    '0x72169649d403b5b512b40d5c2',
-	//    /*  37 */    '0x3d713a141a21a93a218c980c1',
-	//    /*  38 */    '0x215544c77538e6de9275431a6',
-	//    /*  39 */    '0x123c0edc8bf784d147024b7df',
-	//    /*  40 */    '0xa11eada236d9ccb5d9a46757',
-	//    /*  41 */    '0x59f185464ae514ade263ef14',
-	//    /*  42 */    '0x32d507935c586248656e95cb',
-	//    /*  43 */    '0x1d2270a4f18efd8eab5a27d7',
-	//    /*  44 */    '0x10f7bfaf758e3c1010bead08',
-	//    /*  45 */    '0xa101f6bc5df6cc4cf4cb56d',
-	//    /*  46 */    '0x61773c45cb6403833991e6e',
-	//    /*  47 */    '0x3c5f563f3abca8034b91c7d',
-	//    /*  48 */    '0x265cd2a70d374397f75a844',
-	//    /*  49 */    '0x1911bbf62c34780ee22ce8e',
-	//    /*  50 */    '0x10e3053085e97a7710c2e6d',
-	//    /*  51 */    '0xbbfc0e61443560740fa601',
-	//    /*  52 */    '0x874f16aa407949aebced14',
-	//    /*  53 */    '0x64df208d66f55c59261f5d',
-	//    /*  54 */    '0x4dee90487e19a58fbf52e9',
-	//    /*  55 */    '0x3e679f9e3b2f65e9d9b0db',
-	//    /*  56 */    '0x33c719b34c57f9f7a922f6',
-	//    /*  57 */    '0x2c7c090c36927c216fe17c',
-	//    /*  58 */    '0x2789fc1ccdbd02af70650f',
-	//    /*  59 */    '0x2451aae7a1741e150c6ae0',
-	//    /*  60 */    '0x22700f74722225e8c308e6',
-	//    /*  61 */    '0x21aae2600cf1170129eb92',
-	//    /*  62 */    '0x21e552192ec12eccaa1d44',
-	//    /*  63 */    '0x231a0b6c2a250a15897b8a',
-	//    /*  64 */    '0x255901ff2640b9b00fef5e',
-	//    /*  65 */    '0x28c842993fe2877ca68b09',
-	//    /*  66 */    '0x2da7b7138200abf065bc12',
-	//    /*  67 */    '0x34584e19c1677771772dbf',
-	//    /*  68 */    '0x3d678fd12af3f51aa5828a',
-	//    /*  69 */    '0x49a16c994ca36bb50c32c9',
-	//    /*  70 */    '0x5a2b2d67887520aacedab6',
-	//    /*  71 */    '0x70ac191abaee2a72987db6',
-	//    /*  72 */    '0x8f8afbb1a74e96379df7b1',
-	//    /*  73 */    '0xba4bd6d86b43467101fd6c',
-	//    /*  74 */    '0xf61f8e0679ef553e95c271',
-	//    /*  75 */    '0x14ac1e3b06c9771ad8f351c',
-	//    /*  76 */    '0x1c3d320c47b0e10030f080e',
-	//    /*  77 */    '0x272f678a02b5bd5dcc145a7',
-	//    /*  78 */    '0x3732bb25f4914992758a3aa',
-	//    /*  79 */    '0x4ee25a85a30b4e758af15a0',
-	//    /*  80 */    '0x724dbc7344a886ed20dbae2',
-	//    /*  81 */    '0xa7d64de739a14a222daf692',
-	//    /*  82 */    '0xf99876906cf6526b6b82ecc',
-	//    /*  83 */    '0x177bbaca105a36b48757a319',
-	//    /*  84 */    '0x23c442370233418f33964a65',
-	//    /*  85 */    '0x3716c05776b217ecbb587d11',
-	//    /*  86 */    '0x55c42bb597ed985a9d69778e',
-	//    /*  87 */    '0x86e8f9efa6efeba9e16b0a90',
-	//    /*  88 */    '0xd651f2e547d194ee8b6d9a69',
-	//    /*  89 */    '0x157b681e454d31a35819b1989',
-	//    /*  90 */    '0x22c414309a2b397b4f8e0eb28',
-	//    /*  91 */    '0x38c1a2330fcf634a5db1378a0',
-	//    /*  92 */    '0x5d6efaaf8133556840468bbbb',
-	//    /*  93 */    '0x9b0c82dee2e1f20d0a157a7ae',
-	//    /*  94 */    '0x10347bdd997b95a7905d850436',
-	//    /*  95 */    '0x1b4c902e273a586783055cede8',
-	//    /*  96 */    '0x2e50642e85a0b7c589bac2651b',
-	//    /*  97 */    '0x4f1b7f75028232ad3258b8b742',
-	//    /*  98 */    '0x880028111c381b5279db2271c3',
-	//    /*  99 */    '0xeb454460fe475acef6b927865e',
-	//    /* 100 */    '0x1996fab0c95ac4a2b5cfa8f555d',
-	//    /* 101 */    '0x2cc9f3994685c8d3224acb9fea1',
-	//    /* 102 */    '0x4ed2e079d693966878c7149351a',
-	//    /* 103 */    '0x8b740d663b523dad8b67451d8fc',
-	//    /* 104 */    '0xf7f73c5d826e196ff66a259204c',
-	//    /* 105 */    '0x1bb0d7eb2857065dcad087986fa6',
-	//    /* 106 */    '0x31b4dfa1eedd2bd17d3504820344',
-	//    /* 107 */    '0x599fae8ac47c48cf034887f489bb',
-	//    /* 108 */    '0xa249948898a0e444bffa21361f42',
-	//    /* 109 */    '0x12711786051c98ca2acc4adf7ba6a',
-	//    /* 110 */    '0x21a98821bf01e72cc3f724b65a121',
-	//    /* 111 */    '0x3dad0dd7c71f7b443dddd56fede23',
-	//    /* 112 */    '0x716933ca69ac1b439f976665fafdf',
-	//    /* 113 */    '0xd143a4beebca9707458aad7b22dcd',
-	//    /* 114 */    '0x18369cb4cd8522c1b28abc22a3e805',
-	//    /* 115 */    '0x2cf816f46d1971ec18f0ffb6922e86',
-	//    /* 116 */    '0x53c58e5a59ee4d9fd7f747f67a3aac',
-	//    /* 117 */    '0x9c833e3c0364561037250933eab9a9',
-	//    /* 118 */    '0x1253c9d983f03e6a0955355049411cb',
-	//    /* 119 */    '0x226e05852615979ea99f6ef68dbab51',
-	//    /* 120 */    '0x40d8c81134ee9e16db1e0108defbb9f',
-	//    /* 121 */    '0x7a70173a27075f4b9482d36deadc951',
-	//    /* 122 */    '0xe7b966d76665f99c3fb1791404f62c6',
-	//    /* 123 */    '0x1b78e22c38ae6aa69d36b8ccfade23fd',
-	//    /* 124 */    '0x3439aeef615a970c9678397b6ad71179',
-	//    /* 125 */    '0x637d37d6cb204d7419ac094d7e89f0dd',
-	//    /* 126 */    '0xbde80a98943810876a7852209de22be2',
-	//    /* 127 */    '0x16b3160a3c604c6667ff40ff1882b0fcf',
-	//];
+}
+
+func initHelperMaxValArray() {
+	var HelperStrMaxValArray [128]string
+	HelperMaxValArray[0] = "2550a7d99147ce113d27f304d24a422c3d"
+	HelperMaxValArray[1] = "1745f7d567fdd8c93da354496cf4dddf34"
+	HelperMaxValArray[2] = "0b5301cf4bf20167721bcdbe218a66f1e0"
+	HelperMaxValArray[3] = "05e2d2ca56fae9ef2e524ba4d0f75b8754"
+	HelperMaxValArray[4] = "2f45acad795bce6dcd748391bb828dcea"
+	HelperMaxValArray[5] = "17f631b6609d1047920e1a1f9613f870d"
+	HelperMaxValArray[6] = "c29d4a7745ae89ef20a05db656441649"
+	HelperMaxValArray[7] = "6242dea9277cf2d473468985313625bb"
+	HelperMaxValArray[8] = "31aef9b37fbc57d1ca51c53eb472c345"
+	HelperMaxValArray[9] = "1923b23c38638957faeb8b4fe57b5ead"
+	HelperMaxValArray[10] = "cb919ec79bf364210433b9b9680eadd"
+	HelperMaxValArray[11] = "67186c63186761709a96a91d44ff2bf"
+	HelperMaxValArray[12] = "343e6242f854acd626b78022c4a8002"
+	HelperMaxValArray[13] = "1a7efb7b1b687ccb2bb413b92d5e413"
+	HelperMaxValArray[14] = "d72d0627fadb6aa6e0f3c994a5592a"
+	HelperMaxValArray[15] = "6d4f32a7dcd0924c122312b7522049"
+	HelperMaxValArray[16] = "37947990f145344d736c1e7e5cff2f"
+	HelperMaxValArray[17] = "1c49d8ceb31e3ef3e98703e0e656cc"
+	HelperMaxValArray[18] = "e69cb6255a180e2ead170f676fa3c"
+	HelperMaxValArray[19] = "75a24620898b4a19aafdfa67d23e8"
+	HelperMaxValArray[20] = "3c1419351dd33d49e1ce203728e25"
+	HelperMaxValArray[21] = "1eb97e709f819575e656eefb8bd98"
+	HelperMaxValArray[22] = "fbc4a1f867f03d4c057d522b6523"
+	HelperMaxValArray[23] = "812507c14867d2237468ba955def"
+	HelperMaxValArray[24] = "425b9d8ca5a58142d5172c3eb2b5"
+	HelperMaxValArray[25] = "2228e76a368b75ea80882c9f6010"
+	HelperMaxValArray[26] = "119ed9f43c52cdd38348ee8d7b23"
+	HelperMaxValArray[27] = "91bfcff5e91c7f115393af54bad"
+	HelperMaxValArray[28] = "4b8845f19f7b4a93653588ce846"
+	HelperMaxValArray[29] = "273fa600431f30b0f21b619c797"
+	HelperMaxValArray[30] = "1474840ba4069691110ff1bb823"
+	HelperMaxValArray[31] = "ab212322b671a11d3647e3ecaf"
+	HelperMaxValArray[32] = "59ce8876bf3a3b1b396ae19c95"
+	HelperMaxValArray[33] = "2f523e50d3b0d68a3e39f2f06e"
+	HelperMaxValArray[34] = "190c4f51698c5ee5c3b34928a0"
+	HelperMaxValArray[35] = "d537c5d5647f2a79965d56f94"
+	HelperMaxValArray[36] = "72169649d403b5b512b40d5c2"
+	HelperMaxValArray[37] = "3d713a141a21a93a218c980c1"
+	HelperMaxValArray[38] = "215544c77538e6de9275431a6"
+	HelperMaxValArray[39] = "123c0edc8bf784d147024b7df"
+	HelperMaxValArray[40] = "a11eada236d9ccb5d9a46757"
+	HelperMaxValArray[41] = "59f185464ae514ade263ef14"
+	HelperMaxValArray[42] = "32d507935c586248656e95cb"
+	HelperMaxValArray[43] = "1d2270a4f18efd8eab5a27d7"
+	HelperMaxValArray[44] = "10f7bfaf758e3c1010bead08"
+	HelperMaxValArray[45] = "a101f6bc5df6cc4cf4cb56d"
+	HelperMaxValArray[46] = "61773c45cb6403833991e6e"
+	HelperMaxValArray[47] = "3c5f563f3abca8034b91c7d"
+	HelperMaxValArray[48] = "265cd2a70d374397f75a844"
+	HelperMaxValArray[49] = "1911bbf62c34780ee22ce8e"
+	HelperMaxValArray[50] = "10e3053085e97a7710c2e6d"
+	HelperMaxValArray[51] = "bbfc0e61443560740fa601"
+	HelperMaxValArray[52] = "874f16aa407949aebced14"
+	HelperMaxValArray[53] = "64df208d66f55c59261f5d"
+	HelperMaxValArray[54] = "4dee90487e19a58fbf52e9"
+	HelperMaxValArray[55] = "3e679f9e3b2f65e9d9b0db"
+	HelperMaxValArray[56] = "33c719b34c57f9f7a922f6"
+	HelperMaxValArray[57] = "2c7c090c36927c216fe17c"
+	HelperMaxValArray[58] = "2789fc1ccdbd02af70650f"
+	HelperMaxValArray[59] = "2451aae7a1741e150c6ae0"
+	HelperMaxValArray[60] = "22700f74722225e8c308e6"
+	HelperMaxValArray[61] = "21aae2600cf1170129eb92"
+	HelperMaxValArray[62] = "21e552192ec12eccaa1d44"
+	HelperMaxValArray[63] = "231a0b6c2a250a15897b8a"
+	HelperMaxValArray[64] = "255901ff2640b9b00fef5e"
+	HelperMaxValArray[65] = "28c842993fe2877ca68b09"
+	HelperMaxValArray[66] = "2da7b7138200abf065bc12"
+	HelperMaxValArray[67] = "34584e19c1677771772dbf"
+	HelperMaxValArray[68] = "3d678fd12af3f51aa5828a"
+	HelperMaxValArray[69] = "49a16c994ca36bb50c32c9"
+	HelperMaxValArray[70] = "5a2b2d67887520aacedab6"
+	HelperMaxValArray[71] = "70ac191abaee2a72987db6"
+	HelperMaxValArray[72] = "8f8afbb1a74e96379df7b1"
+	HelperMaxValArray[73] = "ba4bd6d86b43467101fd6c"
+	HelperMaxValArray[74] = "f61f8e0679ef553e95c271"
+	HelperMaxValArray[75] = "14ac1e3b06c9771ad8f351c"
+	HelperMaxValArray[76] = "1c3d320c47b0e10030f080e"
+	HelperMaxValArray[77] = "272f678a02b5bd5dcc145a7"
+	HelperMaxValArray[78] = "3732bb25f4914992758a3aa"
+	HelperMaxValArray[79] = "4ee25a85a30b4e758af15a0"
+	HelperMaxValArray[80] = "724dbc7344a886ed20dbae2"
+	HelperMaxValArray[81] = "a7d64de739a14a222daf692"
+	HelperMaxValArray[82] = "f99876906cf6526b6b82ecc"
+	HelperMaxValArray[83] = "177bbaca105a36b48757a319"
+	HelperMaxValArray[84] = "23c442370233418f33964a65"
+	HelperMaxValArray[85] = "3716c05776b217ecbb587d11"
+	HelperMaxValArray[86] = "55c42bb597ed985a9d69778e"
+	HelperMaxValArray[87] = "86e8f9efa6efeba9e16b0a90"
+	HelperMaxValArray[88] = "d651f2e547d194ee8b6d9a69"
+	HelperMaxValArray[89] = "157b681e454d31a35819b1989"
+	HelperMaxValArray[90] = "22c414309a2b397b4f8e0eb28"
+	HelperMaxValArray[91] = "38c1a2330fcf634a5db1378a0"
+	HelperMaxValArray[92] = "5d6efaaf8133556840468bbbb"
+	HelperMaxValArray[93] = "9b0c82dee2e1f20d0a157a7ae"
+	HelperMaxValArray[94] = "10347bdd997b95a7905d850436"
+	HelperMaxValArray[95] = "1b4c902e273a586783055cede8"
+	HelperMaxValArray[96] = "2e50642e85a0b7c589bac2651b"
+	HelperMaxValArray[97] = "4f1b7f75028232ad3258b8b742"
+	HelperMaxValArray[98] = "880028111c381b5279db2271c3"
+	HelperMaxValArray[99] = "eb454460fe475acef6b927865e"
+	HelperMaxValArray[100] = "1996fab0c95ac4a2b5cfa8f555d"
+	HelperMaxValArray[101] = "2cc9f3994685c8d3224acb9fea1"
+	HelperMaxValArray[102] = "4ed2e079d693966878c7149351a"
+	HelperMaxValArray[103] = "8b740d663b523dad8b67451d8fc"
+	HelperMaxValArray[104] = "f7f73c5d826e196ff66a259204c"
+	HelperMaxValArray[105] = "1bb0d7eb2857065dcad087986fa6"
+	HelperMaxValArray[106] = "31b4dfa1eedd2bd17d3504820344"
+	HelperMaxValArray[107] = "599fae8ac47c48cf034887f489bb"
+	HelperMaxValArray[108] = "a249948898a0e444bffa21361f42"
+	HelperMaxValArray[109] = "12711786051c98ca2acc4adf7ba6a"
+	HelperMaxValArray[110] = "21a98821bf01e72cc3f724b65a121"
+	HelperMaxValArray[111] = "3dad0dd7c71f7b443dddd56fede23"
+	HelperMaxValArray[112] = "716933ca69ac1b439f976665fafdf"
+	HelperMaxValArray[113] = "d143a4beebca9707458aad7b22dcd"
+	HelperMaxValArray[114] = "18369cb4cd8522c1b28abc22a3e805"
+	HelperMaxValArray[115] = "2cf816f46d1971ec18f0ffb6922e86"
+	HelperMaxValArray[116] = "53c58e5a59ee4d9fd7f747f67a3aac"
+	HelperMaxValArray[117] = "9c833e3c0364561037250933eab9a9"
+	HelperMaxValArray[118] = "1253c9d983f03e6a0955355049411cb"
+	HelperMaxValArray[119] = "226e05852615979ea99f6ef68dbab51"
+	HelperMaxValArray[120] = "40d8c81134ee9e16db1e0108defbb9f"
+	HelperMaxValArray[121] = "7a70173a27075f4b9482d36deadc951"
+	HelperMaxValArray[122] = "e7b966d76665f99c3fb1791404f62c6"
+	HelperMaxValArray[123] = "1b78e22c38ae6aa69d36b8ccfade23fd"
+	HelperMaxValArray[124] = "3439aeef615a970c9678397b6ad71179"
+	HelperMaxValArray[125] = "637d37d6cb204d7419ac094d7e89f0dd"
+	HelperMaxValArray[126] = "bde80a98943810876a7852209de22be2"
+	HelperMaxValArray[127] = "16b3160a3c604c6667ff40ff1882b0fcf"
+	for i := 0; i < 128; i++ {
+		b, _ := hex.DecodeString(HelperStrMaxValArray[i])
+		HelperMaxValArray[i] = (&big.Int{}).SetBytes(b)
+	}
 }
